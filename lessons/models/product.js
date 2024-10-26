be3f5ddf91b1f211a0b1ff6ddd636e8f233c1b06
abhignya25 <abhignya2002@gmail.com>
2024-10-26 22:23:13 +0530

    enhancing the app

--- lessons/models/product.js
+++ lessons/models/product.js
@@ -7,32 +7,34 @@ const p = path.join(
   'products.json'
 );
 
-
-const getProductFromFile = cb => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
-      return cb([]);
+      cb([]);
     } else {
-    cb(JSON.parse(fileContent));
+      cb(JSON.parse(fileContent));
     }
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductFromFile(products => {
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
     });
   }
 
   static fetchAll(cb) {
-    getProductFromFile(cb)
+    getProductsFromFile(cb);
   }
 };
